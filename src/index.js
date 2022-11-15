8f135521ac781b4e3cf04eb143719edcdd72134a
BigBadTubaDude <cralex0307@gmail.com>
2022-11-15 11:09:37 -0500

    renamed roots

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import PaintApp from './'
 import reportWebVitals from './reportWebVitals';
 
 //Flash Test data input page
-const flashRoot = ReactDOM.createRoot(document.getElementById('flash'));
-flashRoot.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <FlashApp />
   </React.StrictMode>
