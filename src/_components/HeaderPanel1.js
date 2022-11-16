8d89a666eb80425753a1e4d2f404e1d6448df3ae
BigBadTubaDude <cralex0307@gmail.com>
2022-11-16 13:35:19 -0500

    Paint page now shows temp while flash does not stopped bar type h1 appearance from moving temp/humidity inputs

--- src/_components/HeaderPanel1.js
+++ src/_components/HeaderPanel1.js
@@ -4,6 +4,8 @@ import React from 'react';
 import ANTButton from "./ANTRackResetButton.js";
 import DipSprayToggle from './DipSprayToggle.js'
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 export default function HeaderPanel1(props) {
 
     const widthList = [
@@ -32,188 +34,195 @@ export default function HeaderPanel1(props) {
         )
     })
     return (
-        <header className='panel1Header'>
-        <div className='P1HeaderQuarter1'>
-            <MaterialToggle 
-                currentMaterialType={props.currentMaterialType} 
-                setCurrentMaterialType={props.setCurrentMaterialType}
-                currentBarType={props.currentBarType}
-            />
-            <DipSprayToggle 
-                currentDipSprayType={props.currentDipSprayType} 
-                setCurrentDipSprayType={props.setCurrentDipSprayType}
-            />            
-        </div>
-        <div className='P1HeaderQuarter2'>
-             <div className='temp'>  
-                <h2>Temp</h2>
-                <div>
-                    <input  
-                        className={props.currentTemp == ""
-                            ? "incompleteField"
-                            : ""
-                        }
-                        onChange={props.changeTempState} 
-                        pattern="\d*" 
-                        maxLength="3" 
-                        type="text" 
-                    />
-                    <p>F</p>
+        <Router>
+
+            <header className='panel1Header'>
+            <div className='P1HeaderQuarter1'>
+                <MaterialToggle 
+                    currentMaterialType={props.currentMaterialType} 
+                    setCurrentMaterialType={props.setCurrentMaterialType}
+                    currentBarType={props.currentBarType}
+                />
+                <DipSprayToggle 
+                    currentDipSprayType={props.currentDipSprayType} 
+                    setCurrentDipSprayType={props.setCurrentDipSprayType}
+                />            
+            </div>
+            <div className='P1HeaderQuarter2'>
+                    <Switch>
+                        <Route exact path="/paint">
+                            <div className='temp'>  
+                                <h2>Temp</h2>
+                                <div>
+                                    <input  
+                                        className={props.currentTemp == ""
+                                            ? "incompleteField"
+                                            : ""
+                                        }
+                                        onChange={props.changeTempState} 
+                                        pattern="\d*" 
+                                        maxLength="3" 
+                                        type="text" 
+                                    />
+                                    <p>F</p>
+                                </div>
+                            </div> 
+                        </Route>
+                    </Switch>
+                
+                <div className='humidity'>
+                    <h2>Humidity</h2>
+                    <div>
+                        <input 
+                            onChange={props.changeHumidityState} 
+                            className={props.currentHumidity == ""
+                                ? "incompleteFieldExtra"
+                                : ""
+                            }
+                            pattern="\d*" 
+                            maxLength="3" 
+                            type="text" 
+                        />
+                        <p>%</p>
+                    </div>
                 </div>
-               
-            </div> 
-            <div className='humidity'>
-                <h2>Humidity</h2>
-                <div>
-                    <input 
-                        onChange={props.changeHumidityState} 
-                        className={props.currentHumidity == ""
+                <h1 className='barTypeh1'>{props.currentBarType}</h1>
+            </div>
+            <div className='P1HeaderQuarter3'>
+                <div className='widthSelectContainer'>
+                    <h3>Width</h3>
+                    <select 
+                        onChange={props.changeWidthState}
+                        className={props.currentWidth == ""
                             ? "incompleteFieldExtra"
                             : ""
                         }
-                        pattern="\d*" 
-                        maxLength="3" 
-                        type="text" 
+                    >
+                        {WidthOptionsHTML}
+                    </select>
+                </div>
+                <div className='defectCountSection'>   
+                    <h3 className='withLabel'>with</h3>
+                    <input 
+                        type="number" 
+                        name="materialInput" 
+                        onChange={changeDefectCountState}  
+                        className='currentDefectCountInput'
+                        min={1}
+                        readOnly
+                        value={props.currentDefectCountDisplay}
                     />
-                    <p>%</p>
+                    <h3>defects</h3>
                 </div>
             </div>
-            <h1 className='barTypeh1'>{props.currentBarType}</h1>
-        </div>
-        <div className='P1HeaderQuarter3'>
-            <div className='widthSelectContainer'>
-                <h3>Width</h3>
-                <select 
-                    onChange={props.changeWidthState}
-                    className={props.currentWidth == ""
-                        ? "incompleteFieldExtra"
-                        : ""
-                    }
-                >
-                    {WidthOptionsHTML}
-                </select>
-            </div>
-            <div className='defectCountSection'>   
-                <h3 className='withLabel'>with</h3>
-                <input 
-                    type="number" 
-                    name="materialInput" 
-                    onChange={changeDefectCountState}  
-                    className='currentDefectCountInput'
-                    min={1}
-                    readOnly
-                    value={props.currentDefectCountDisplay}
-                />
-                <h3>defects</h3>
-            </div>
-        </div>
 
 
-        <div className='rackPositionSection'>
-        <h3>{`Phase ${props.currentPhaseSelected}`}</h3>
-        <div 
-            className={props.currentRackPosition == ""
-                ? " phaseRadioButtons"
-                :"phaseRadioButtons" 
-            }
-            onChange={props.changePhaseState}
-        >  
-                <input 
-                    type="radio" 
-                    id="L1" 
-                    value="L1" 
-                    name="phase" 
-                    className={props.currentPhaseSelected == ""
-                        ? "phaseRadioButton barTypeRadios incompleteField"
-                        : "phaseRadioButton barTypeRadios"
-                    }
-               />
-                <label 
-                    htmlFor="L1">
-                    L1
-                </label>
-                <br />          
-                <input 
-                    type="radio" 
-                    id="L2" 
-                    value="L2" 
-                    name="phase"
-                    className={props.currentPhaseSelected == ""
-                        ? "phaseRadioButton barTypeRadios incompleteField"
-                        : "phaseRadioButton barTypeRadios"
-                    }
+            <div className='rackPositionSection'>
+            <h3>{`Phase ${props.currentPhaseSelected}`}</h3>
+            <div 
+                className={props.currentRackPosition == ""
+                    ? " phaseRadioButtons"
+                    :"phaseRadioButtons" 
+                }
+                onChange={props.changePhaseState}
+            >  
+                    <input 
+                        type="radio" 
+                        id="L1" 
+                        value="L1" 
+                        name="phase" 
+                        className={props.currentPhaseSelected == ""
+                            ? "phaseRadioButton barTypeRadios incompleteField"
+                            : "phaseRadioButton barTypeRadios"
+                        }
                 />
-                <label 
-                    htmlFor="L2">
-                    L2
-                </label>
-                <br />
-                <input 
-                    type="radio" 
-                    id="L3" 
-                    value="L3" 
-                    name="phase" 
-                    className={props.currentPhaseSelected == ""
-                        ? "phaseRadioButton barTypeRadios incompleteField"
-                        : "phaseRadioButton barTypeRadios"
-                    }
-                />
-                <label 
-                    htmlFor="L3">
-                    L3
-                </label>
-                <br />
-                <input 
-                    type="radio" 
-                    id="N" 
-                    value="N" 
-                    name="phase" 
-                    className={props.currentPhaseSelected == ""
-                        ? "phaseRadioButton barTypeRadios incompleteField"
-                        : "phaseRadioButton barTypeRadios"
-                    }
-                />
-                <label 
-                    htmlFor="N">
-                    N
-                </label>
-                <br />
-                <input 
-                    type="radio" 
-                    id="E" 
-                    value="E" 
-                    name="phase" 
-                    className={props.currentPhaseSelected == ""
-                        ? "phaseRadioButton barTypeRadios incompleteField"
-                        : "phaseRadioButton barTypeRadios"
-                    }
-                />
-                <label 
-                    htmlFor="E">
-                    E
-                </label>
-                <br />
+                    <label 
+                        htmlFor="L1">
+                        L1
+                    </label>
+                    <br />          
+                    <input 
+                        type="radio" 
+                        id="L2" 
+                        value="L2" 
+                        name="phase"
+                        className={props.currentPhaseSelected == ""
+                            ? "phaseRadioButton barTypeRadios incompleteField"
+                            : "phaseRadioButton barTypeRadios"
+                        }
+                    />
+                    <label 
+                        htmlFor="L2">
+                        L2
+                    </label>
+                    <br />
+                    <input 
+                        type="radio" 
+                        id="L3" 
+                        value="L3" 
+                        name="phase" 
+                        className={props.currentPhaseSelected == ""
+                            ? "phaseRadioButton barTypeRadios incompleteField"
+                            : "phaseRadioButton barTypeRadios"
+                        }
+                    />
+                    <label 
+                        htmlFor="L3">
+                        L3
+                    </label>
+                    <br />
+                    <input 
+                        type="radio" 
+                        id="N" 
+                        value="N" 
+                        name="phase" 
+                        className={props.currentPhaseSelected == ""
+                            ? "phaseRadioButton barTypeRadios incompleteField"
+                            : "phaseRadioButton barTypeRadios"
+                        }
+                    />
+                    <label 
+                        htmlFor="N">
+                        N
+                    </label>
+                    <br />
+                    <input 
+                        type="radio" 
+                        id="E" 
+                        value="E" 
+                        name="phase" 
+                        className={props.currentPhaseSelected == ""
+                            ? "phaseRadioButton barTypeRadios incompleteField"
+                            : "phaseRadioButton barTypeRadios"
+                        }
+                    />
+                    <label 
+                        htmlFor="E">
+                        E
+                    </label>
+                    <br />
+                </div>
+                <ANTButton 
+                    resetRadioButtons={props.resetRadioButtons}
+                    currentPhaseState={props.currentPhaseState}
+                    setCurrentPhaseSelected={props.setCurrentPhaseSelected}
+                    setCurrentRackPosition={props.setCurrentRackPosition}
+                    currentRackPosition={props.currentRackPosition}
+                    />
+                    <h3>{`Rack ${props.currentRackPosition}`}</h3>
+                <div className="rackPositionRadioButtons" onChange={props.changeRackState}>  
+                    <input type="radio" id="1" value="1" name="rackPosition" className="rackPositionRadioButton barTypeRadios"></input>
+                    <label htmlFor="1">1</label><br />          
+                    <input type="radio" id="2" value="2" name="rackPosition" className="rackPositionRadioButton barTypeRadios"></input>
+                    <label htmlFor="2">2</label><br />
+                    <input type="radio" id="3" value="3" name="rackPosition" className="rackPositionRadioButton barTypeRadios"></input>
+                    <label htmlFor="3">3</label><br />
+                    <input type="radio" id="4" value="4" name="rackPosition" className="rackPositionRadioButton barTypeRadios"></input>
+                    <label htmlFor="4">4</label><br />
+                </div>
             </div>
-            <ANTButton 
-                resetRadioButtons={props.resetRadioButtons}
-                currentPhaseState={props.currentPhaseState}
-                setCurrentPhaseSelected={props.setCurrentPhaseSelected}
-                setCurrentRackPosition={props.setCurrentRackPosition}
-                currentRackPosition={props.currentRackPosition}
-                />
-                <h3>{`Rack ${props.currentRackPosition}`}</h3>
-            <div className="rackPositionRadioButtons" onChange={props.changeRackState}>  
-                <input type="radio" id="1" value="1" name="rackPosition" className="rackPositionRadioButton barTypeRadios"></input>
-                <label htmlFor="1">1</label><br />          
-                <input type="radio" id="2" value="2" name="rackPosition" className="rackPositionRadioButton barTypeRadios"></input>
-                <label htmlFor="2">2</label><br />
-                <input type="radio" id="3" value="3" name="rackPosition" className="rackPositionRadioButton barTypeRadios"></input>
-                <label htmlFor="3">3</label><br />
-                <input type="radio" id="4" value="4" name="rackPosition" className="rackPositionRadioButton barTypeRadios"></input>
-                <label htmlFor="4">4</label><br />
-            </div>
-        </div>
 
-        </header>
+            </header>
+        </Router>
     )
 }
